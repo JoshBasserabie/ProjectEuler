8e04d124af9ef3477aa9a7f0ab8dd9730835bc94
JoshBasserabie <josh.basserabie@gmail.com>
2018-07-17 10:18:24 +1000

    Improved readability

--- 3.py
+++ 3.py
@@ -7,7 +7,7 @@ What is the largest prime factor of the number 600851475143 ?
 def main():
     cap = 600851475143
     max_divisor = 1
-    while cap != 1:
+    while cap > 1:
         for i in range(2, int(cap) + 1):
             if cap % i == 0:
                 cap /= i
